e445b7a3539758bec0feabbd5bd5bf648b1073ff
Yash Sehgal Clamp <yash.sehgal@joinclamp.com>
2023-03-17 04:11:47 +0530

    Add discussion manager using  #1

--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -5,7 +5,8 @@ interface NavbarProps extends React.HTMLAttributes<HTMLDivElement> { };
 
 export default function Navbar(htmlAttributes: NavbarProps) {
     return (
-        <nav className="py-3 border-b border-gray-200 flex flex-row items-center justify-between" {...htmlAttributes}>
+        <nav className="py-3 border-b border-gray-200 flex flex-row items-center justify-between
+            w-full mx-auto max-xl:w-[920px] max-lg:w-[740px] max-md:w-[600px] max-sm:w-[420px]" {...htmlAttributes}>
             <span className="nav-logo-wrapper">
                 <Link href={"/"}>
                     <h1 className="nav-logo-text">{"Governance Forum"}</h1>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,10 @@
 import Button from "@/components/core/Button";
+import DiscussionsWrapper from "@/components/ui/DiscussionsManager";
 
 export default function MainView() {
   return (
     <main className="main-view">
+      <DiscussionsWrapper />
     </main>
   );
 }
